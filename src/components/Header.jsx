c43426241b9b46160a648c51f5c2fb1f5610b649
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-11-23 00:18:43 +0530

    header and pnf page completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { IoIosLogOut } from "react-icons/io";
+import logoImg from "../assets/logo-128.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div className="bg-primary d-flex justify-content-between align-items-center px-3 py-3">
+        <div className="d-flex align-items-center column-gap-4">
+          <div className="">
+            <h4 className="text-white">Work Tracker</h4>
+            <p className="text-white">Do it on time !</p>
+          </div>
+        </div>
+        <div className="">
+          <button
+            type="button"
+            className="btn btn-primary d-flex gap-2 align-items-center"
+          >
+            <IoIosLogOut style={{ fontSize: "28px" }} />
+            Logout
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Pnf.jsx
+++ src/pages/Pnf.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { CiWarning } from 'react-icons/ci'
 
 const Pnf = () => {
   return (
-    <div>PAGE NOT FOUND</div>
+    <div className='text-center text-primary my-5 d-flex flex-column justify-content-center align-items-center'><CiWarning style={{fontSize:'100px'}} /><h1>PAGE NOT FOUND</h1></div>
   )
 }
 
