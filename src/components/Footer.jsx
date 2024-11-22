884d5117d739eee93791c395dd3b8706804da147
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-11-23 00:27:45 +0530

    footer  completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <div className="w-100 bg-primary text-white text-center px-2 py-3">
+          <p>&copy; All Rights Reserved. Designed and Developed by <a href="https://sarangpachari.netlify.app/" target='_blank' className='text-decoration-none text-white'>Sarang P Achari</a></p>
+      </div>
+    </>
   )
 }
 
