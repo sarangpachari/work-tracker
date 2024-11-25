f488366bbfbcb2b85a810c30f468436b2249379a
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-11-25 20:30:33 +0530

    loading setted only for signup,login,dashboard

--- src/pages/AttendedWorks.jsx
+++ src/pages/AttendedWorks.jsx
@@ -5,7 +5,6 @@ import { db } from "../config/firebase";
 import { IoIosArrowBack } from "react-icons/io";
 import Toast from "react-bootstrap/Toast";
 import ToastContainer from "react-bootstrap/ToastContainer";
-import LoadingSpinner from "../components/LoadingSpinner";
 
 const AttendedWorks = () => {
   const { id } = useParams();
@@ -18,22 +17,18 @@ const AttendedWorks = () => {
     customerPhone: "",
   });
   const [toast, setToast] = useState({ show: false, message: "", variant: "" });
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchData();
   }, [allAttendedWorks]);
 
   const fetchData = async () => {
-    setLoading(true);
     const docRef = doc(db, "users", id);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       const data = docSnap.data();
       setAllAttendedWorks(data.allAttendedWorks);
-      setLoading(false);
     }
-    
   };
 
   const onSave = async (e, wid) => {
@@ -154,110 +149,106 @@ const AttendedWorks = () => {
 
   return (
     <>
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <div className="m-3">
-          <Link to={`/dashboard/${id}`} style={{ textDecoration: "none" }}>
-            <button className="btn btn-warning d-flex align-items-center gap-2 my-4">
-              <IoIosArrowBack style={{ fontSize: "24px" }} /> Back to Dashboard
-            </button>
-          </Link>
-          <h3>All Attended Works</h3>
+      <div className="m-3">
+        <Link to={`/dashboard/${id}`} style={{ textDecoration: "none" }}>
+          <button className="btn btn-warning d-flex align-items-center gap-2 my-4">
+            <IoIosArrowBack style={{ fontSize: "24px" }} /> Back to Dashboard
+          </button>
+        </Link>
+        <h3>All Attended Works</h3>
 
-          {/* Toast Container */}
-          <ToastContainer position="top-end" className="p-3">
-            <Toast
-              onClose={() => setToast({ ...toast, show: false })}
-              show={toast.show}
-              delay={3000}
-              autohide
-              bg={toast.variant === "success" ? "success" : "danger"}
-            >
-              <Toast.Body className="text-white">{toast.message}</Toast.Body>
-            </Toast>
-          </ToastContainer>
+        {/* Toast Container */}
+        <ToastContainer position="top-end" className="p-3">
+          <Toast
+            onClose={() => setToast({ ...toast, show: false })}
+            show={toast.show}
+            delay={3000}
+            autohide
+            bg={toast.variant === "success" ? "success" : "danger"}
+          >
+            <Toast.Body className="text-white">{toast.message}</Toast.Body>
+          </Toast>
+        </ToastContainer>
 
-          {/* CARDS */}
-          <div className="d-flex flex-wrap gap-3 p-3">
-            {allAttendedWorks?.length > 0 ? (
-              allAttendedWorks?.map((work, index) => (
-                <div
-                  key={index}
-                  className="card border-danger mb-3"
-                  style={{ width: "25rem" }}
-                >
-                  <div className="card-header">
-                    Created :{" "}
-                    {work?.createdAt?.toDate().toLocaleString("en-US", {
-                      hour12: true,
-                    })}{" "}
-                    <br />
-                    Last Edited :{" "}
-                    {work?.lastEdited?.toDate().toLocaleString("en-US", {
-                      hour12: true,
-                    })}
-                    <br />
-                    Attended :{" "}
-                    {work?.attendedAt?.toDate().toLocaleString("en-US", {
-                      hour12: true,
-                    })}
-                  </div>
-                  <div className="card-body">
-                    <h4 className="card-title">{work?.workTitle}</h4>
-                    <p className="card-text">Location : {work?.workLocation}</p>
-                    <p className="card-text">
-                      Customer Name : {work?.customerName}
-                    </p>
-                    <p className="card-text">
-                      Customer Phone : {work?.customerPhone}
-                    </p>
-                    <textarea
-                      type="text"
-                      className="form-control"
-                      placeholder="Add Work Description"
-                      defaultValue={work?.workDescription}
-                      onChange={(e) =>
-                        setWorkDetails({
-                          ...workDetails,
-                          workDescription: e.target.value,
-                          createdAt: work?.createdAt,
-                          attendedAt: work?.attendedAt,
-                          workTitle: work?.workTitle,
-                          workLocation: work?.workLocation,
-                          customerName: work?.customerName,
-                          customerPhone: work?.customerPhone,
-                          lastEdited: new Date(),
-                        })
-                      }
-                    />
-                    <div className="d-flex flex-wrap gap-2 mt-3">
-                      <button
-                        onClick={(e) => onSave(e, work?.id)}
-                        type="button"
-                        className="btn btn-outline-danger w-100"
-                      >
-                        Save
-                      </button>
-                      <button
-                        onClick={(e) => markAsCompleted(e, work)}
-                        type="button"
-                        className="btn btn-success w-100"
-                      >
-                        Mark as Completed
-                      </button>
-                    </div>
+        {/* CARDS */}
+        <div className="d-flex flex-wrap gap-3 p-3">
+          {allAttendedWorks?.length > 0 ? (
+            allAttendedWorks?.map((work, index) => (
+              <div
+                key={index}
+                className="card border-danger mb-3"
+                style={{ width: "25rem" }}
+              >
+                <div className="card-header">
+                  Created :{" "}
+                  {work?.createdAt?.toDate().toLocaleString("en-US", {
+                    hour12: true,
+                  })}{" "}
+                  <br />
+                  Last Edited :{" "}
+                  {work?.lastEdited?.toDate().toLocaleString("en-US", {
+                    hour12: true,
+                  })}
+                  <br />
+                  Attended :{" "}
+                  {work?.attendedAt?.toDate().toLocaleString("en-US", {
+                    hour12: true,
+                  })}
+                </div>
+                <div className="card-body">
+                  <h4 className="card-title">{work?.workTitle}</h4>
+                  <p className="card-text">Location : {work?.workLocation}</p>
+                  <p className="card-text">
+                    Customer Name : {work?.customerName}
+                  </p>
+                  <p className="card-text">
+                    Customer Phone : {work?.customerPhone}
+                  </p>
+                  <textarea
+                    type="text"
+                    className="form-control"
+                    placeholder="Add Work Description"
+                    defaultValue={work?.workDescription}
+                    onChange={(e) =>
+                      setWorkDetails({
+                        ...workDetails,
+                        workDescription: e.target.value,
+                        createdAt: work?.createdAt,
+                        attendedAt: work?.attendedAt,
+                        workTitle: work?.workTitle,
+                        workLocation: work?.workLocation,
+                        customerName: work?.customerName,
+                        customerPhone: work?.customerPhone,
+                        lastEdited: new Date(),
+                      })
+                    }
+                  />
+                  <div className="d-flex flex-wrap gap-2 mt-3">
+                    <button
+                      onClick={(e) => onSave(e, work?.id)}
+                      type="button"
+                      className="btn btn-outline-danger w-100"
+                    >
+                      Save
+                    </button>
+                    <button
+                      onClick={(e) => markAsCompleted(e, work)}
+                      type="button"
+                      className="btn btn-success w-100"
+                    >
+                      Mark as Completed
+                    </button>
                   </div>
                 </div>
-              ))
-            ) : (
-              <div className="alert alert-danger" role="alert">
-                No Attended works found
               </div>
-            )}
-          </div>
+            ))
+          ) : (
+            <div className="alert alert-danger" role="alert">
+              No Attended works found
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 };
